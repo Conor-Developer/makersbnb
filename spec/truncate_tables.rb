8206971bf69ee76c87a17812dfbb0caa47cadfb3
nadia <nadiabenzineb1@gmail.com>
2022-04-20 17:19:43 +0100

    backend listing of property complete

--- spec/truncate_tables.rb
+++ spec/truncate_tables.rb
@@ -2,5 +2,5 @@ require 'pg'
 
 def truncate_tables
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec('TRUNCATE TABLE users;')
+  connection.exec('TRUNCATE TABLE users,properties;')
 end
